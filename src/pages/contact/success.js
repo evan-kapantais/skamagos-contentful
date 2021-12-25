717b261150a5ae0dbcfb9b484bf2dfd66cb9fd16
evan-kapantais <evankapantais@gmail.com>
2021-12-25 17:23:20 +0200

    responsive success page

--- src/pages/contact/success.js
+++ src/pages/contact/success.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { graphql, Link } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
-
 import Seo from '../../components/seo';
-
-import instagram from '../../images/instagram-black.svg';
-import facebook from '../../images/facebook-black.svg';
+import Social from '../../components/Social';
 
 import * as styles from './success.module.css';
 
 const ContactPage = ({ data }) => {
   const nodes = data.allContentfulProject.nodes;
 
-  const nodes1 = nodes.slice(0, nodes.length / 3);
-  const nodes2 = nodes.slice(nodes.length / 3, nodes.length - nodes.length / 3);
-  const nodes3 = nodes.slice(nodes.length - nodes.length / 3, nodes.length);
+  const nodes1 = nodes.slice(0, nodes.length / 2);
+  const nodes2 = nodes.slice(nodes.length / 2);
 
   return (
     <div className={styles.page}>
@@ -22,80 +18,67 @@ const ContactPage = ({ data }) => {
       <header>
         <nav>
           <ul>
-            <li className={styles.socialItem}>
+            <li>
               <Link to="/">Home</Link>
             </li>
           </ul>
         </nav>
       </header>
       <main>
+        <div className={styles.grid}>
+          <ul className={styles.column}>
+            {nodes1.map((node, i) => (
+              <li key={node.contentful_id}>
+                <Link to={`/${node.slug}`}>
+                  <GatsbyImage
+                    key={i}
+                    image={node.heroImage.gatsbyImageData}
+                    alt={node.title}
+                    title={node.title}
+                    className={styles.image}
+                  />
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <ul className={styles.column}>
+            {nodes2.map((node, i) => (
+              <li key={node.contentful_id}>
+                <Link to={`/${node.slug}`}>
+                  <GatsbyImage
+                    key={i}
+                    image={node.heroImage.gatsbyImageData}
+                    alt={node.title}
+                    title={node.title}
+                    className={styles.image}
+                  />
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <ul className={styles.row}>
+          {nodes.map((node, i) => (
+            <li key={node.contentful_id}>
+              <Link to={`/${node.slug}`}>
+                <GatsbyImage
+                  key={i}
+                  image={node.heroImage.gatsbyImageData}
+                  alt={node.title}
+                  title={node.title}
+                  className={styles.image}
+                />
+              </Link>
+            </li>
+          ))}
+        </ul>
         <div className={styles.container}>
           <h1>Thanks for reaching out!</h1>
           <p>I'll get back to you as soon as possible.</p>
         </div>
-        <div className={styles.grid}>
-          <div>
-            {nodes1.map((node, i) => (
-              <GatsbyImage
-                key={i}
-                image={node.heroImage.gatsbyImageData}
-                alt="project image"
-                className={styles.image}
-              />
-            ))}
-          </div>
-          <div>
-            {nodes2.map((node, i) => (
-              <GatsbyImage
-                key={i}
-                image={node.heroImage.gatsbyImageData}
-                alt="project image"
-                className={styles.image}
-              />
-            ))}
-          </div>
-          <div>
-            {nodes3.map((node, i) => (
-              <GatsbyImage
-                key={i}
-                image={node.heroImage.gatsbyImageData}
-                alt="project image"
-                className={styles.image}
-              />
-            ))}
-          </div>
-        </div>
       </main>
       <footer>
-        <ul>
-          <li className={styles.socialItem}>
-            <a
-              href="https://www.instagram.com/konstantinoskm_/"
-              target="_blank"
-              rel="noreferrer"
-              className={styles.socialLink}
-            >
-              <img
-                src={instagram}
-                alt="instagram icon"
-                aria-label="social icon"
-              />
-            </a>
-          </li>
-          <li className={styles.socialItem}>
-            <a
-              href="https://www.facebook.com/skamskami"
-              rel="noreferrer"
-              className={styles.socialLink}
-            >
-              <img
-                src={facebook}
-                alt="facebook icon"
-                aria-label="social icon"
-              />
-            </a>
-          </li>
-        </ul>
+        <Social />
       </footer>
     </div>
   );
@@ -107,6 +90,8 @@ export const query = graphql`
   query successQuery {
     allContentfulProject {
       nodes {
+        title
+        slug
         heroImage {
           gatsbyImageData(width: 400)
         }
