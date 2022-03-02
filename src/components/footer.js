af1b27cf992b8355dedf6e4078f4a814f9395e63
evan-kapantais <evankapantais@gmail.com>
2022-03-02 20:10:30 +0200

    adjust font size, add copy

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,10 +4,15 @@ import * as styles from '../style/footer.module.css';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    Designed and built by{' '}
-    <a href="https://evankapantais.com/" className="text-link">
-      Evan Kapantais_
-    </a>
+    <div>
+      <p>&copy; Konstantinos Skamagos, {new Date().getFullYear()}</p>
+    </div>
+    <div>
+      Designed and built by{' '}
+      <a href="https://evankapantais.com/" className="text-link">
+        Evan Kapantais_
+      </a>
+    </div>
   </footer>
 );
 
