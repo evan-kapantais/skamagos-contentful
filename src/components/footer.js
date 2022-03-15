19fd8c7a3d9ceea7c38d8beda2cc64a1d50f39ee
evan-kapantais <evankapantais@gmail.com>
2022-03-15 23:52:51 +0200

    adjust footer on small screens

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,15 +4,13 @@ import * as styles from '../style/footer.module.css';
 
 const Footer = () => (
   <footer className={styles.footer}>
-    <div>
-      <p>&copy; Konstantinos Skamagos, {new Date().getFullYear()}</p>
-    </div>
-    <div>
+    <p>&copy; Konstantinos Skamagos, {new Date().getFullYear()}</p>
+    <p>
       Designed and built by{' '}
       <a href="https://evankapantais.com/" className="text-link">
         Evan Kapantais_
       </a>
-    </div>
+    </p>
   </footer>
 );
 
